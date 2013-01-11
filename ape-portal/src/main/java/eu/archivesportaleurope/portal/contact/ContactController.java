3bdeb72dae8ef4c6bb4363b537a486b63d881900
yoannmoranville <yoann@archivesportaleurope.net>
2013-01-11 11:14:38 +0000

    Finish contact form portlet
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@210 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/contact/ContactController.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/contact/ContactController.java
@@ -3,14 +3,20 @@ package eu.archivesportaleurope.portal.contact;
 import eu.archivesportaleurope.portal.common.email.EmailSender;
 import eu.archivesportaleurope.portal.search.simple.SimpleSearch;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 import org.springframework.web.portlet.bind.annotation.ResourceMapping;
 
+import javax.portlet.ActionResponse;
+import javax.portlet.RenderResponse;
 import javax.portlet.ResourceRequest;
 
 /**
@@ -42,18 +48,31 @@ public class ContactController {
         return new Contact();
     }
 
-    @ResourceMapping(value = "contact")
-    public ModelAndView showResult(@ModelAttribute("contact") Contact contact, BindingResult result, ResourceRequest request) {
-        ModelAndView modelAndView = new ModelAndView();
+    @RenderMapping(params = "myaction=success")
+    public String showPageResult(RenderResponse response, Model model) {
+        return "success";
+    }
+
+    @RenderMapping(params = "myaction=error")
+    public String showPageError(RenderResponse response, Model model) {
+        return "error";
+    }
+
+
+    @ActionMapping(params = "myaction=contact")
+    public void showResult(@ModelAttribute("contact") Contact contact, BindingResult result, ActionResponse response) {
+        ContactValidator contactValidator = new ContactValidator();
+        contactValidator.validate(contact, result);
+        if(result.hasErrors()) {
+            response.setRenderParameter("myaction", "contact");
+            return;
+        }
         try {
             EmailSender.sendEmail(contact.getType(), contact.getEmail(), contact.getFeedback());
-            modelAndView.setViewName("redirect:contact?success=true");
-//            modelAndView.getModelMap().addAttribute("result", true);
+            response.setRenderParameter("myaction", "success");
         } catch (Exception e) {
             LOG.error("Error sending the email", e);
-            modelAndView.setViewName("redirect:contact?success=false");
-//            modelAndView.getModelMap().addAttribute("result", false);
+            response.setRenderParameter("myaction", "error");
         }
-        return modelAndView;
     }
 }
