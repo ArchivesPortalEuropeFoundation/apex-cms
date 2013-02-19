e0f0b2eeac1793e678240831199b31dfe8abc093
yoannmoranville <yoann@archivesportaleurope.net>
2013-02-19 14:04:42 +0000

    Merged from liferay-portal-project-1.1.1 Issue 266: fix slow navigated tree [from revision 249] Issue 266: fix slow navigated tree [from revision 250] Changes of email addresses [from revision 251]
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@256 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/advanced/altree/ArchivalLandscapeTreeJSONWriter.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/advanced/altree/ArchivalLandscapeTreeJSONWriter.java
@@ -128,11 +128,11 @@ public class ArchivalLandscapeTreeJSONWriter extends AbstractJSONWriter {
 									generateEadFolderTreeJSON(eads, parentType, aiId, start, locale),
 									resourceResponse);
 						} else if (AlType.FINDING_AID.equals(parentType)) {
-							List<? extends Ead> eads = findingAidDAO.getFindingAidsNotLinkedByArchivalInstitution(aiId,
-									start, MAX_NUMBER_OF_EADS + 1);
-							writeToResponseAndClose(
-									generateEadFolderTreeJSON(eads, parentType, aiId, start, locale),
-									resourceResponse);
+//							List<? extends Ead> eads = findingAidDAO.getFindingAidsNotLinkedByArchivalInstitution(aiId,
+//									start, MAX_NUMBER_OF_EADS + 1);
+//							writeToResponseAndClose(
+//									generateEadFolderTreeJSON(eads, parentType, aiId, start, locale),
+//									resourceResponse);
 						}
 
 					} else {
@@ -215,7 +215,7 @@ public class ArchivalLandscapeTreeJSONWriter extends AbstractJSONWriter {
 		 */
 		eadSearchOptions.setEadClazz(SourceGuide.class);
 		boolean sourceGuideExists = eadDAO.existEads(eadSearchOptions);
-		boolean otherFindingAidExists = findingAidDAO.existFindingAidsNotLinkedByArchivalInstitution(aiId);
+		//boolean otherFindingAidExists = findingAidDAO.existFindingAidsNotLinkedByArchivalInstitution(aiId);
 		if (holdingsGuideExists) {
 			AlTreeNode hgNode = new AlTreeNode();
 			addTitleFromKey(hgNode, "text.holdings.guide.folder", locale);
@@ -233,14 +233,14 @@ public class ArchivalLandscapeTreeJSONWriter extends AbstractJSONWriter {
 			alTreeNodes.add(sgNode);
 		}
 		// Adding the Other Finding Aid folder and children
-		if (otherFindingAidExists) {
-			AlTreeNode faNode = new AlTreeNode();
-			addTitleFromKey(faNode, "text.other.finding.aid.folder", locale);
-			faNode.setFolder(true);
-			faNode.setHideCheckbox(true);
-			addKey(faNode, AlType.FINDING_AID, aiId, TreeType.GROUP);
-			alTreeNodes.add(faNode);
-		}
+//		if (otherFindingAidExists) {
+//			AlTreeNode faNode = new AlTreeNode();
+//			addTitleFromKey(faNode, "text.other.finding.aid.folder", locale);
+//			faNode.setFolder(true);
+//			faNode.setHideCheckbox(true);
+//			addKey(faNode, AlType.FINDING_AID, aiId, TreeType.GROUP);
+//			alTreeNodes.add(faNode);
+//		}
 		return alTreeNodes;
 	}
 
