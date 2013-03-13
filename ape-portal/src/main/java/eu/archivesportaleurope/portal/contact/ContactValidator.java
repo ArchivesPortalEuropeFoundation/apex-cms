ae5bbf474ae301b424268862181deb83a678341f
eduardo <eduardo@archivesportaleurope.net>
2013-03-13 09:41:22 +0000

    Bug #300
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@286 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/contact/ContactValidator.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/contact/ContactValidator.java
@@ -27,11 +27,13 @@ public class ContactValidator implements Validator {
             errors.rejectValue("type", "feedback.error.type");
         }
         String email = contact.getEmail();
-        //RFC regexp for emails
-        Pattern pattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-        Matcher matcher = pattern.matcher(email);
-        if(!matcher.find()) {
-            errors.rejectValue("email", "feedback.error.email");
+        if(!email.isEmpty()){ //it's rejected in preview check(rejectIfEmptyOrWhitespace), prevent repeated messages
+        	//RFC regexp for emails
+            Pattern pattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+            Matcher matcher = pattern.matcher(email);
+            if(!matcher.find()) {
+                errors.rejectValue("email", "feedback.error.email");
+            }
         }
     }
 }
