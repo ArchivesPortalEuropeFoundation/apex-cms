dbe8defe5e54ec4260d396b87dd65bee8315210e
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-12 06:56:58 +0000

    better formatting of huge numbers
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.3@536 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/simple/SimpleSearchController.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/simple/SimpleSearchController.java
@@ -1,8 +1,11 @@
 package eu.archivesportaleurope.portal.search.simple;
 
-import javax.portlet.ActionResponse;
+import java.text.NumberFormat;
 
-import org.apache.log4j.Logger;
+import javax.portlet.ActionResponse;
+import javax.portlet.RenderRequest;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,22 +16,29 @@ import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
 import eu.apenet.persistence.dao.EadDAO;
-import eu.apenet.persistence.factory.DAOFactory;
 
 @Controller(value = "simpleSearchController")
 @RequestMapping(value = "VIEW")
 public class SimpleSearchController {
-	private final static Logger LOGGER = Logger.getLogger(SimpleSearchController.class);
+	//private final static Logger LOGGER = Logger.getLogger(SimpleSearchController.class);
 	private ArchivalInstitutionDAO archivalInstitutionDAO;
 	private EadDAO eadDAO;
 	// --maps the incoming portlet request to this method
 	@RenderMapping
-	public ModelAndView showSimpleSearch() {
+	public ModelAndView showSimpleSearch(RenderRequest request) {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("home");
 
 		modelAndView.getModelMap().addAttribute("institutions", archivalInstitutionDAO.countArchivalInstitutionsWithContentIndexed());
-		modelAndView.getModelMap().addAttribute("units", eadDAO.getTotalCountOfUnits());
+		NumberFormat numberFormat = NumberFormat.getInstance(request.getLocale());
+		modelAndView.getModelMap().addAttribute("units", numberFormat.format(eadDAO.getTotalCountOfUnits()));
+		String numberOfDaoUnitsString= request.getPreferences().getValue("numberOfDaoUnits", "0");
+		long numberOfDaoUnits = 0;
+		if (StringUtils.isNotBlank(numberOfDaoUnitsString)){
+			numberOfDaoUnits = Long.parseLong(numberOfDaoUnitsString);
+			
+		}
+		modelAndView.getModelMap().addAttribute("numberOfDaoUnits", numberFormat.format(numberOfDaoUnits));
 		return modelAndView;
 	}
 	@ModelAttribute("simpleSearch")
