1375cca8e934d4145d861e0e6b5f183ab91ebaa5
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-23 12:50:46 +0000

    IssueID 667: google analytics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.3@586 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-theme/src/main/webapp/js/global.js
+++ ape-theme/src/main/webapp/js/global.js
@@ -1,29 +1,3 @@
-loadGoogleAnalytics();
-
-function loadGoogleAnalytics() {
-	var gId = 'UA-42421913-1';
-	var hostname = window.location.hostname;
-	if (hostname == "www.archivesportaleurope.net"
-			|| hostname == "archivesportaleurope.net") {
-		gId = 'UA-37520081-1';
-	} else if (hostname == "contentchecker.archivesportaleurope.net") {
-		gId = 'UA-35748576-1';
-	}
-	var _gaq = _gaq || [];
-	_gaq.push([ '_setAccount', gId ]);
-	_gaq.push([ '_trackPageview' ]);
-
-	(function() {
-		var ga = document.createElement('script');
-		ga.type = 'text/javascript';
-		ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl'
-				: 'http://www')
-				+ '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(ga, s);
-	})();
-}
 
 function logAction(title, url) {
 	var baseUrl = window.location.protocol + '//' + window.location.hostname;
