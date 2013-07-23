68a8ad2631e0dd9ec28a36a41bb5292ce247f55a
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-23 09:34:29 +0000

    IssueID 667: google analytics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.3@585 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-theme/src/main/webapp/js/global.js
+++ ape-theme/src/main/webapp/js/global.js
@@ -29,6 +29,4 @@ function logAction(title, url) {
 	var baseUrl = window.location.protocol + '//' + window.location.hostname;
 	var shortUrl = url.replace(baseUrl, "");
 	_gaq.push([ '_trackPageview', shortUrl ]);
-	alert("yes");
-
 }
