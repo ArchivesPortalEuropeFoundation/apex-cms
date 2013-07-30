e09398c73348e042eb3253e9f0a7117567ea9ef0
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-30 12:21:19 +0000

    merge from branch to trunk
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@592 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-theme/src/main/webapp/js/global.js
+++ ape-theme/src/main/webapp/js/global.js
@@ -1,30 +1,6 @@
-$(document).ready(function() {
-	loadGoogleAnalytics();
-});
 
-function loadGoogleAnalytics() {
-	var hostname = window.location.hostname;
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	if (hostname == "www.archivesportaleurope.net" || hostname == "archivesportaleurope.net"){
-		ga('create', 'UA-37520081-1', 'archivesportaleurope.net');
-	}else if (hostname == "contentchecker.archivesportaleurope.net"){
-		ga('create', 'UA-35748576-1', 'contentchecker.archivesportaleurope.net');
-	}else {
-		ga('create', 'UA-42421913-1', 'development.archivesportaleurope.net');
-	}
-	ga('send', 'pageview', window.location.pathname);
-}
-
-function logAction(title, url){
-	var baseUrl =  window.location.protocol + '//' + window.location.hostname;
+function logAction(title, url) {
+	var baseUrl = window.location.protocol + '//' + window.location.hostname;
 	var shortUrl = url.replace(baseUrl, "");
-	ga('send', 'pageview', {
-		  'page': shortUrl,
-		  'title': url	
-	});
-	
+	_gaq.push([ '_trackPageview', shortUrl ]);
 }
