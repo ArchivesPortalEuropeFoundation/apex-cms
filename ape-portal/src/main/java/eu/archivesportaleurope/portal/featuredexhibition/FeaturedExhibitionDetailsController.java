9530f6797f5ce59bfd26c3fa99e443f68e329d01
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-08-08 12:43:19 +0000

    IssueID 195: Implementation of Featured Exhibition
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@630 8f6216c4-6850-4946-a993-29be1cf876c3

---
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/featuredexhibition/FeaturedExhibitionDetailsController.java
@@ -0,0 +1,64 @@
+package eu.archivesportaleurope.portal.featuredexhibition;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.RenderRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalArticleDisplay;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portlet.journal.util.comparator.ArticleModifiedDateComparator;
+
+@Controller(value = "featuredExhibitionDetailsController")
+@RequestMapping(value = "VIEW")
+public class FeaturedExhibitionDetailsController {
+	private final static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+	private final static Logger LOGGER = Logger.getLogger(FeaturedExhibitionDetailsController.class);
+
+	@RenderMapping
+	public ModelAndView showFeaturedExhibition(RenderRequest request) {
+		LOGGER.info("classPK: " + request.getParameter("classPK"));
+		LOGGER.info("articleId: " + request.getParameter("articleId"));
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("details-index");
+		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+		String articleId = request.getParameter("articleId");
+		String classPK = request.getParameter("classPK");
+		String templateId = request.getPreferences().getValue("webContentTemplateId", "");
+		
+		String articleContent = null;
+		try {
+			if (StringUtils.isNumeric(classPK)){
+				long classPkLong = Long.parseLong(classPK);
+				JournalArticle journalArticle = JournalArticleLocalServiceUtil.getLatestArticle(classPkLong);
+				JournalArticleDisplay articleDisplay = JournalArticleLocalServiceUtil.getArticleDisplay(
+						themeDisplay.getScopeGroupId(), journalArticle.getArticleId() , "view", themeDisplay.getLanguageId(), themeDisplay);
+				articleContent = articleDisplay.getContent();
+			}else if (StringUtils.isNotBlank(articleId)){
+				JournalArticleDisplay article = JournalArticleLocalServiceUtil.getArticleDisplay(themeDisplay.getScopeGroupId(), articleId,"view", themeDisplay.getLanguageId(), themeDisplay);
+				if (article == null){
+					articleContent = "Article does not exists";
+				}else {
+					articleContent = article.getContent();
+				}
+			}
+
+		} catch (Exception e) {
+			articleContent = "Article does not exists";
+		}
+		modelAndView.addObject("articleDetails", articleContent);
+		return modelAndView;
+	}
+
+}
