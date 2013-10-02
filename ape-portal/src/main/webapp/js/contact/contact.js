e9efcc5ea325d12544c347a307e303548866904d
fernando <fernando@archivesportaleurope.net>
2013-10-02 16:01:40 +0000

    Feature #750 - WEB 2.0 - User's feedback
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@876 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/webapp/js/contact/contact.js
+++ ape-portal/src/main/webapp/js/contact/contact.js
@@ -3,3 +3,19 @@ function initContactForm() {
         window.location.href = "/home";
     });
 }
+
+function getFile(){
+	//this gets the full url
+	var url = document.location.href;
+
+	if(parseInt(url.indexOf("?"))>1){
+		//if url contains "?" this means this comes from "User feedback"
+		url = url.substring(url.indexOf("?")+1, (url.length));
+		document.getElementById('contact_feedbackText').value = unescape(url);
+		//print the title of the document in the feedback text area
+		//->contact_feedbackText.innerHTML = decodeURIComponent(url);
+		//select the option in the dropdown and set Users feedback
+		var select = document.getElementById("contact_topicSubject");
+        select.options[4].selected = true;
+	}	
+}
