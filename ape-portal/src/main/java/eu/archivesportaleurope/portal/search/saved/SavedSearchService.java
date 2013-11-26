4327d4d25a371d9132b02141919feae3fcf8c6c1
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-11-26 12:28:30 +0000

    IssueID 14: Implement saved searches
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@998 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/saved/SavedSearchService.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/saved/SavedSearchService.java
@@ -2,6 +2,8 @@ package eu.archivesportaleurope.portal.search.saved;
 
 import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
+
 import eu.apenet.persistence.dao.EadSavedSearchDAO;
 import eu.apenet.persistence.vo.EadSavedSearch;
 import eu.archivesportaleurope.portal.search.advanced.AdvancedSearch;
@@ -18,10 +20,25 @@ public class SavedSearchService {
 		EadSavedSearch eadSavedSearch = new EadSavedSearch();
 		eadSavedSearch.setLiferayUserId(liferayUserId);
 		eadSavedSearch.setModifiedDate(new Date());
-		eadSavedSearch.setSearchTerm(advancedSearch.getTerm());
+		eadSavedSearch.setSearchTerm(removeEmptyString(advancedSearch.getTerm()));
 		eadSavedSearch.setHierarchy(AdvancedSearch.VIEW_HIERARCHY.equals(advancedSearch.getView()));
 		eadSavedSearch.setMethodOptional(AdvancedSearch.METHOD_OPTIONAL.equals(advancedSearch.getMethod()));
 		eadSavedSearch.setOnlyWithDaos(TRUE.equals(advancedSearch.getDao()));
+		/*
+		 * advanced search options
+		 */
+		eadSavedSearch.setElement(removeEmptyString(advancedSearch.getElement()));
+		eadSavedSearch.setTypedocument(removeEmptyString(advancedSearch.getTypedocument()));
+		eadSavedSearch.setFromdate(removeEmptyString(advancedSearch.getFromdate()));
+		eadSavedSearch.setTodate(removeEmptyString(advancedSearch.getTodate()));
+		eadSavedSearch.setExactDateSearch(TRUE.equals(advancedSearch.getExactDateSearch()));
+		
+		/*
+		 * refinements
+		 */
+		eadSavedSearch.setRefinementCountry(removeEmptyString(advancedSearch.getCountry()));
+		eadSavedSearch.setRefinementAi(removeEmptyString(advancedSearch.getAi()));
+		eadSavedSearch.setRefinementFacetSettings(removeEmptyString(advancedSearch.getFacetSettings()));
 		eadSavedSearchDAO.store(eadSavedSearch);
 	}
 	
@@ -42,9 +59,28 @@ public class SavedSearchService {
 			if (eadSavedSearch.isOnlyWithDaos()){
 				advancedSearch.setDao(TRUE);
 			}
+			advancedSearch.setElement(eadSavedSearch.getElement());
+			advancedSearch.setTypedocument(eadSavedSearch.getTypedocument());
+			advancedSearch.setFromdate(eadSavedSearch.getFromdate());
+			advancedSearch.setTodate(eadSavedSearch.getTodate());
+			if (eadSavedSearch.isExactDateSearch()){
+				advancedSearch.setExactDateSearch(TRUE);
+			}
+			if (!eadSavedSearch.isTemplate()){
+				advancedSearch.setCountry(eadSavedSearch.getRefinementCountry());
+				advancedSearch.setAi(eadSavedSearch.getRefinementAi());
+				advancedSearch.setFacetSettings(eadSavedSearch.getRefinementFacetSettings());
+			}
 			return advancedSearch;
 		}else {
 			return null;
 		}		
+	}
+	private static String removeEmptyString(String string){
+		if (StringUtils.isBlank(string)){
+			return null;
+		}else {
+			return string;
+		}
 	}
 }
