5f6634491efe0e26b89c4c14d6be228a11225d23
yoannmoranville <yoann@archivesportaleurope.net>
2013-12-04 16:59:57 +0000

    Merged from frontend-project-1.5 Servlet send directly the last version nb of DPT so DPT can download it directly. [from revision 1054] Forgot merge from branch 1.4 to trunk [from revision 1055]
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1056 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/servlets/DptUpdateServlet.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/servlets/DptUpdateServlet.java
@@ -1,12 +1,14 @@
 package eu.archivesportaleurope.portal.servlets;
 
 import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.DptUpdate;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
  * User: Yoann Moranville
@@ -23,12 +25,18 @@ public class DptUpdateServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
         try {
             String versionNb = request.getParameter("versionNb");
-            if(StringUtils.isEmpty(versionNb) || !DAOFactory.instance().getDptUpdateDAO().doesVersionExist(versionNb)) {
+            DptUpdate dptUpdate = DAOFactory.instance().getDptUpdateDAO().doesVersionExist(versionNb);
+            if(StringUtils.isEmpty(versionNb) || dptUpdate == null) {
                 LOG.info("No update for " + versionNb);
                 response.sendError(404);
             } else {
-                LOG.info("Need update for " + versionNb);
-                response.sendError(200);
+                LOG.info("Need update for " + versionNb + " with version " + dptUpdate.getNewVersion());
+                response.setHeader("Content-Type", "text/plain");
+                response.setHeader("success", "yes");
+                PrintWriter writer = response.getWriter();
+                writer.write(dptUpdate.getNewVersion());
+                writer.flush();
+                writer.close();
             }
         } catch (Exception e) {
             LOG.error("error...", e);
