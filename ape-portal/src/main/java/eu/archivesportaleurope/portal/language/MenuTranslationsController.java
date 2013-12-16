4df67fb0eb08d0d47e33cd67dd3cae5e74f0a4c1
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-12-16 08:57:07 +0000

    IssueID 974 Menu translations
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.5@1100 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/language/MenuTranslationsController.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/language/MenuTranslationsController.java
@@ -3,23 +3,21 @@ package eu.archivesportaleurope.portal.language;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletRequest;
 import javax.portlet.RenderResponse;
 
-import eu.archivesportaleurope.portal.common.email.EmailSender;
-import eu.archivesportaleurope.portal.contact.Contact;
-import eu.archivesportaleurope.portal.contact.ContactValidator;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.portlet.ModelAndView;
 import org.springframework.web.portlet.bind.annotation.ActionMapping;
@@ -43,7 +41,10 @@ import com.liferay.portal.theme.ThemeDisplay;
 @RequestMapping(value = "VIEW")
 public class MenuTranslationsController {
 	private final static Logger LOGGER = Logger.getLogger(MenuTranslationsController.class);
-
+	private ResourceBundleMessageSource messageSource;
+	public void setMessageSource(ResourceBundleMessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@RenderMapping
     public ModelAndView chooseLayout(PortletRequest portletRequest) throws SystemException {
@@ -82,7 +83,67 @@ public class MenuTranslationsController {
         modelAndView.setViewName("index");
         return modelAndView;
     }
+    
+    @ActionMapping(params = "myaction=cleanTranslations")
+	public void cleanTranslations(ActionRequest actionRequest, ActionResponse response) {
+    	try {
+        ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+        long groupId = themeDisplay.getLayout().getGroupId();
+        List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(groupId, false);
+        Locale defaultLocale = themeDisplay.getCompany().getLocale();
+        LOGGER.info("Default locale: " + defaultLocale);
+        for (Layout layout: layouts){
+        	String url = layout.getFriendlyURL().substring(1);
+        	Map<Locale, String> nameMap = layout.getNameMap();
+        	for (Locale locale: nameMap.keySet()){
+        		if (!locale.equals(defaultLocale)){
+        			LOGGER.info ("Page " + url + " remove translation " + locale);
+        			layout.setName(null, locale);
 
+        		}
+        	}
+        	LayoutLocalServiceUtil.updateLayout(layout);
+        }
+    	}catch (Exception e){
+    		LOGGER.error(e.getMessage(), e);
+    	}
+	}
+    @ActionMapping(params = "myaction=updateTranslations")
+	public void updateTranslations(ActionRequest actionRequest, ActionResponse response) {
+    	try {
+        ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+        long groupId = themeDisplay.getLayout().getGroupId();
+        List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(groupId, false);
+        Locale defaultLocale = themeDisplay.getCompany().getLocale();
+        Locale.setDefault(defaultLocale);
+        LOGGER.info("Default locale: " + defaultLocale);
+        for (Layout layout: layouts){
+        	String url = layout.getFriendlyURL().substring(1);
+    		String resourcePropertyName = "menu." + url;
+        	Locale[] locales = LanguageUtil.getAvailableLocales();
+        	String defaultLocaleMenuName = "";
+        	try{
+        		defaultLocaleMenuName = messageSource.getMessage(resourcePropertyName, null, defaultLocale);
+			}catch (NoSuchMessageException e){
+				LOGGER.error("No translation "+ defaultLocale + ": " +resourcePropertyName);
+			}
+        	for (Locale locale: locales){
+
+        		try {
+					String localizedMenuName = messageSource.getMessage(resourcePropertyName, null, locale);
+	        		if (StringUtils.isNotBlank(localizedMenuName) && !localizedMenuName.equals(defaultLocaleMenuName)){
+	        			LOGGER.info ("Page " + url + " add translation " + locale);
+	        			layout.setName(localizedMenuName, locale);       			
+	        		}
+        		}catch (NoSuchMessageException e){
+        		}
+        	}
+        	LayoutLocalServiceUtil.updateLayout(layout);
+        }
+    	}catch (Exception e){
+    		LOGGER.error(e.getMessage(), e);
+    	}
+	}
     @ActionMapping(params = "myaction=display")
 	public void displayLanguage(PortletRequest portletRequest, ActionResponse response, @RequestParam(value="layout", required = false) String layout) throws SystemException {
         response.setRenderParameter("myaction", "display");
