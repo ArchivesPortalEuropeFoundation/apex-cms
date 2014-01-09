f017d252912e1e53332dc92e15eda9290e6b1ee7
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-01-09 10:35:00 +0000

    IssueID 976: Selected criteria shown
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.5@1152 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/advanced/Refinement.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/advanced/Refinement.java
@@ -5,16 +5,24 @@ public class Refinement {
 	private String fieldValue;
 	private String fieldName;
 	private String longDescription;
+	private boolean removed;
 	
 	public Refinement(String fieldName, Integer fieldValue, String longDescription) {
 		this (fieldName, fieldValue+"", longDescription);
 	}
 	
+	public Refinement(String fieldName, Integer fieldValue, String longDescription, boolean removed) {
+		this (fieldName, fieldValue+"", longDescription,removed);
+	}
 	public Refinement(String fieldName, String fieldValue, String longDescription) {
+		this (fieldName, fieldValue+"", longDescription, false);
+	}
+	public Refinement(String fieldName, String fieldValue, String longDescription, boolean removed) {
 		super();
 		this.fieldName = fieldName;
 		this.fieldValue = fieldValue;
-		this.longDescription = longDescription.replaceAll("'", "&#039");;
+		this.longDescription = longDescription.replaceAll("'", "&#039");
+		this.removed = removed;
 	}
 	public String getFieldValue() {
 		return fieldValue;
@@ -25,5 +33,11 @@ public class Refinement {
 	public String getLongDescription() {
 		return longDescription;
 	}
-	
+	public String getCssClass(){
+		if (removed){
+			return "removed";
+		}else {
+			return "";
+		}
+	}
 }
--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/saved/SavedSearchService.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/saved/SavedSearchService.java
@@ -10,11 +10,18 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
+import eu.apenet.commons.types.XmlType;
+import eu.apenet.commons.utils.DisplayUtils;
 import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
 import eu.apenet.persistence.dao.CountryDAO;
+import eu.apenet.persistence.dao.EadDAO;
 import eu.apenet.persistence.dao.EadSavedSearchDAO;
+import eu.apenet.persistence.dao.EadSearchOptions;
 import eu.apenet.persistence.vo.ArchivalInstitution;
+import eu.apenet.persistence.vo.Country;
+import eu.apenet.persistence.vo.Ead;
 import eu.apenet.persistence.vo.EadSavedSearch;
+import eu.archivesportaleurope.portal.common.SpringResourceBundleSource;
 import eu.archivesportaleurope.portal.search.advanced.AdvancedSearch;
 import eu.archivesportaleurope.portal.search.advanced.Refinement;
 import eu.archivesportaleurope.portal.search.common.FacetType;
@@ -26,6 +33,7 @@ public class SavedSearchService {
 	private EadSavedSearchDAO eadSavedSearchDAO;
 	private CountryDAO countryDAO;
 	private ArchivalInstitutionDAO archivalInstitutionDAO;
+	private EadDAO eadDAO;
 	private ResourceBundleMessageSource messageSource;
 
 	public void setEadSavedSearchDAO(EadSavedSearchDAO eadSavedSearchDAO) {
@@ -34,6 +42,11 @@ public class SavedSearchService {
 
 	public void setCountryDAO(CountryDAO countryDAO) {
 		this.countryDAO = countryDAO;
+	}
+
+	
+	public void setEadDAO(EadDAO eadDAO) {
+		this.eadDAO = eadDAO;
 	}
 
 	public void setMessageSource(ResourceBundleMessageSource messageSource) {
@@ -77,9 +90,12 @@ public class SavedSearchService {
 		/*
 		 * refinements
 		 */
-		eadSavedSearch.setRefinementCountry(removeEmptyString(advancedSearch.getCountry()));
-		eadSavedSearch.setRefinementAi(removeEmptyString(advancedSearch.getAi()));
-		eadSavedSearch.setRefinementFond(removeEmptyString(advancedSearch.getFond()));
+		if (StringUtils.isBlank(advancedSearch.getFond())){
+			eadSavedSearch.setRefinementCountry(removeEmptyString(advancedSearch.getCountry()));
+			eadSavedSearch.setRefinementAi(removeEmptyString(advancedSearch.getAi()));			
+		}else {
+			eadSavedSearch.setRefinementFond(removeEmptyString(advancedSearch.getFond()));
+		}
 		eadSavedSearch.setRefinementType(removeEmptyString(advancedSearch.getType()));
 		eadSavedSearch.setRefinementLevel(removeEmptyString(advancedSearch.getLevel()));
 		eadSavedSearch.setRefinementDao(removeEmptyString(advancedSearch.getDao()));
@@ -159,8 +175,20 @@ public class SavedSearchService {
 
 	public List<Refinement> convertToRefinements(PortletRequest request, AdvancedSearch advancedSearch,
 			EadSavedSearch eadSavedSearch) {
+		SpringResourceBundleSource source = new SpringResourceBundleSource(messageSource, request.getLocale());
 		List<Refinement> refinements = new ArrayList<Refinement>();
 		try {
+			List<Integer> countryIds = convertToIntegers(advancedSearch.getCountryList());
+			List<Country> countries = countryDAO.getCountries(countryIds);
+			for (Country country : countries) {
+				String localizedName = DisplayUtils.getLocalizedCountryName(source, country);
+				refinements.add(new Refinement(FacetType.COUNTRY.getName(), country.getId(), localizedName));
+				removeInteger(countryIds, country.getId());
+			}
+			for (Integer countryId : countryIds) {
+				String countryName = source.getString("advancedsearch.text.savesearch.removedcountry");
+				refinements.add(new Refinement(FacetType.COUNTRY.getName(), countryId, countryName, true));
+			}
 			List<Integer> aiIds = convertToIntegers(advancedSearch.getAiList());
 			List<ArchivalInstitution> archivalInstitutions = archivalInstitutionDAO.getArchivalInstitutions(aiIds);
 			for (ArchivalInstitution archivalInstitution : archivalInstitutions) {
@@ -169,9 +197,53 @@ public class SavedSearchService {
 				removeInteger(aiIds, archivalInstitution.getAiId());
 			}
 			for (Integer aiId : aiIds) {
-				String aiName = messageSource.getMessage("advancedsearch.text.savesearch.removedai", null,
-						request.getLocale());
-				refinements.add(new Refinement(FacetType.AI.getName(), aiId, aiName));
+				String aiName = source.getString("advancedsearch.text.savesearch.removedai");
+				refinements.add(new Refinement(FacetType.AI.getName(), aiId, aiName, true));
+			}
+			if (StringUtils.isNotBlank(advancedSearch.getFond()) && advancedSearch.getFond().length() > 1){
+				XmlType xmlType = XmlType.getTypeBySolrPrefix(advancedSearch.getFond().substring(0,1));
+				String identifier = advancedSearch.getFond().substring(1);
+				Integer id = Integer.parseInt(identifier);
+				EadSearchOptions eadSearchOptions = new EadSearchOptions();
+				eadSearchOptions.setEadClass(xmlType.getClazz());
+				eadSearchOptions.setId(id);
+				List<Ead> eads = eadDAO.getEads(eadSearchOptions);
+				if (eads.size() > 0){
+					Ead ead = eads.get(0);
+					refinements.add(new Refinement(FacetType.FOND.getName(), advancedSearch.getFond(), ead.getTitle()));					
+				}else {
+					String fondName = source.getString("advancedsearch.text.savesearch.removedfond");
+					refinements.add(new Refinement(FacetType.FOND.getName(), advancedSearch.getFond(), fondName, true));
+				}
+				
+			}
+			List<String> types = advancedSearch.getTypeList();
+			if (types != null){
+				for (String type : types) {
+					refinements.add(new Refinement(FacetType.TYPE.getName(), type,
+							 source.getString(FacetType.TYPE.getPrefix()+type)));
+				}				
+			}
+			List<String> levels = advancedSearch.getLevelList();
+			if (levels != null){
+				for (String level : levels) {
+					refinements.add(new Refinement(FacetType.LEVEL.getName(), level,
+							 source.getString(FacetType.LEVEL.getPrefix()+level)));
+				}				
+			}
+			List<String> daos = advancedSearch.getDaoList();
+			if (daos != null){
+				for (String dao : daos) {
+					refinements.add(new Refinement(FacetType.DAO.getName(), dao,
+							 source.getString(FacetType.DAO.getPrefix()+dao)));
+				}				
+			}
+			List<String> dateTypes = advancedSearch.getDateTypeList();
+			if (dateTypes != null){
+				for (String dateType : dateTypes) {
+					refinements.add(new Refinement(FacetType.DATE_TYPE.getName(), dateType,
+							 source.getString(FacetType.DATE_TYPE.getPrefix()+dateType)));
+				}				
 			}
 		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
@@ -182,23 +254,26 @@ public class SavedSearchService {
 
 	private static List<Integer> convertToIntegers(List<String> strings) {
 		List<Integer> integers = new ArrayList<Integer>();
-		for (String temp : strings) {
-			if (StringUtils.isNumeric(temp)) {
-				integers.add(Integer.parseInt(temp));
+		if (strings != null) {
+			for (String temp : strings) {
+				if (StringUtils.isNumeric(temp)) {
+					integers.add(Integer.parseInt(temp));
+				}
 			}
 		}
 		return integers;
 	}
 
-	private static void removeInteger(List<Integer> list, Integer value){
+	private static void removeInteger(List<Integer> list, Integer value) {
 		boolean found = false;
-		for (int i=0;!found &&  i < list.size();i++){
-			if (value.equals(list.get(i))){
+		for (int i = 0; !found && i < list.size(); i++) {
+			if (value.equals(list.get(i))) {
 				list.remove(i);
 				found = true;
 			}
 		}
 	}
+
 	private static String removeEmptyString(String string) {
 		if (StringUtils.isBlank(string)) {
 			return null;
