12680d0b4ffe815a4e34eb2bef5fc37a9dd89104
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-01-31 13:22:19 +0000

    remove special characters.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.5@1216 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/common/PortalDisplayUtil.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/common/PortalDisplayUtil.java
@@ -25,11 +25,14 @@ public class PortalDisplayUtil {
     public static final String TITLE_SIMPLE_SEARCH = "ADVANCED SEARCH";
     public static final String TITLE_FEATURED_DOCUMENT = "FEATURED DOCUMENTS";
     public static final String TITLE_SAVED_SEARCH = "SAVED SEARCHES";
+    public static void main(String args[]){
+    	System.out.println(replaceQuotesAndReturns("taba\t \\%\\; algo   por \\p algo2 \n go \r con \r\n más \\ con /"));
+    }
 	public static String replaceQuotesAndReturns(String string) {
 		String result = string;
 		if (result != null) {
 			result = result.replaceAll("\"", "'");
-			result = result.replaceAll("[\n\t\r]", "");
+			result = result.replaceAll("[\n\t\r\\\\/%;]", "");
 			result = result.trim();
 		}
 		return result;
