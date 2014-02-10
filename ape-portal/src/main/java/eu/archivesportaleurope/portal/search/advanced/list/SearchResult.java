c1b1da422e0d9fcdc6ad24f4615239a3a35ed726
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-02-10 13:35:52 +0000

    IssueID 833
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1250 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/advanced/list/SearchResult.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/advanced/list/SearchResult.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.apache.solr.common.SolrDocument;
 
-import eu.apenet.commons.solr.SolrFields;
+import eu.apenet.commons.solr.eads.EadSolrFields;
 import eu.apenet.commons.utils.DisplayUtils;
 import eu.archivesportaleurope.portal.search.common.AdvancedSearchUtil;
 
@@ -39,41 +39,41 @@ public class SearchResult {
 
 	public SearchResult (SolrDocument solrDocument, Map<String, Map<String, List<String>>> highlightingMap){
 		this.solrDocument = solrDocument;
-		id = solrDocument.getFieldValue( SolrFields.ID).toString();
+		id = solrDocument.getFieldValue( EadSolrFields.ID).toString();
 		String titleWithoutEscaping = null;
-		if (solrDocument.getFieldValue(SolrFields.ALTERDATE) != null){
-			String alterdateWithoutEscaping  = solrDocument.getFieldValue(SolrFields.ALTERDATE).toString();
-			String highlightedAlterdate =  AdvancedSearchUtil.getHighlightedString(highlightingMap, id, SolrFields.ALTERDATE, alterdateWithoutEscaping);
+		if (solrDocument.getFieldValue(EadSolrFields.ALTERDATE) != null){
+			String alterdateWithoutEscaping  = solrDocument.getFieldValue(EadSolrFields.ALTERDATE).toString();
+			String highlightedAlterdate =  AdvancedSearchUtil.getHighlightedString(highlightingMap, id, EadSolrFields.ALTERDATE, alterdateWithoutEscaping);
 			this.alterdate = DisplayUtils.encodeHtmlWithHighlighting(highlightedAlterdate);			
 			this.alterdateWithoutHighlighting = DisplayUtils.encodeHtml(alterdateWithoutEscaping);
 		}
-		if (solrDocument.getFieldValue(SolrFields.TITLE) != null){
-			titleWithoutEscaping = solrDocument.getFieldValue(SolrFields.TITLE).toString();
-			String highlightedTitle =  AdvancedSearchUtil.getHighlightedString(highlightingMap, id, SolrFields.TITLE, titleWithoutEscaping);
+		if (solrDocument.getFieldValue(EadSolrFields.TITLE) != null){
+			titleWithoutEscaping = solrDocument.getFieldValue(EadSolrFields.TITLE).toString();
+			String highlightedTitle =  AdvancedSearchUtil.getHighlightedString(highlightingMap, id, EadSolrFields.TITLE, titleWithoutEscaping);
 			this.title = DisplayUtils.encodeHtmlWithHighlighting(highlightedTitle);
 			this.titleWithoutHighlighting = DisplayUtils.encodeHtml(titleWithoutEscaping);
 		}		
 
 
 		
-		this.scopecontent =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, SolrFields.SCOPECONTENT, null));	
-		this.other =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, SolrFields.OTHER, null));
-		this.fond = solrDocument.getFieldValue(SolrFields.TITLE_OF_FOND).toString();
+		this.scopecontent =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, EadSolrFields.SCOPECONTENT, null));	
+		this.other =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, EadSolrFields.OTHER, null));
+		this.fond = solrDocument.getFieldValue(EadSolrFields.TITLE_OF_FOND).toString();
 		this.fondId = getIdFromString(this.fond);
 		this.fond = getDescriptionFromString(this.fond);
-		this.ai = solrDocument.getFieldValue(SolrFields.AI).toString();
+		this.ai = solrDocument.getFieldValue(EadSolrFields.AI).toString();
 		this.aiId = getIdFromString(this.ai);
 		this.ai = getDescriptionFromString(this.ai);
-		if (solrDocument.getFieldValue(SolrFields.UNITID) != null){
-			this.unitid  = solrDocument.getFieldValue(SolrFields.UNITID).toString();
+		if (solrDocument.getFieldValue(EadSolrFields.UNITID) != null){
+			this.unitid  = solrDocument.getFieldValue(EadSolrFields.UNITID).toString();
 		}
-		this.unitid =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, SolrFields.UNITID, unitid));
-		this.otherUnitid =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, SolrFields.OTHERUNITID, null));
+		this.unitid =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, EadSolrFields.UNITID, unitid));
+		this.otherUnitid =  DisplayUtils.encodeHtmlWithHighlighting(AdvancedSearchUtil.getHighlightedString(highlightingMap, id, EadSolrFields.OTHERUNITID, null));
 		if (otherUnitid != null){
 			this.otherUnitid = "(" + this.otherUnitid + ")";
 		}
-		if (solrDocument.getFieldValue(SolrFields.LEVEL) != null)
-			this.level = solrDocument.getFieldValue(SolrFields.LEVEL).toString();
+		if (solrDocument.getFieldValue(EadSolrFields.LEVEL) != null)
+			this.level = solrDocument.getFieldValue(EadSolrFields.LEVEL).toString();
 	}
 
 	public String getId() {
@@ -106,23 +106,23 @@ public class SearchResult {
 	}
 
 	public Object getCountry(){
-		String country = solrDocument.getFieldValue(SolrFields.COUNTRY).toString();
+		String country = solrDocument.getFieldValue(EadSolrFields.COUNTRY).toString();
 		return getDescriptionFromString(country);
 	}
 	public Object getAi(){
 		return ai;
 	}
 	public String getType(){
-		return solrDocument.getFieldValue(SolrFields.TYPE).toString();
+		return solrDocument.getFieldValue(EadSolrFields.TYPE).toString();
 	}
 	public Object getDao(){
-		return solrDocument.getFieldValue(SolrFields.DAO);
+		return solrDocument.getFieldValue(EadSolrFields.DAO);
 	}
 	public Object getRoledao(){
-		return solrDocument.getFieldValue(SolrFields.ROLEDAO);
+		return solrDocument.getFieldValue(EadSolrFields.ROLEDAO);
 	}
 	public Object getUnitIdOfFond(){
-		return solrDocument.getFieldValue(SolrFields.UNITID_OF_FOND);
+		return solrDocument.getFieldValue(EadSolrFields.UNITID_OF_FOND);
 	}
 	private String getDescriptionFromString(String string){
 		int index = string.indexOf(COLON);
