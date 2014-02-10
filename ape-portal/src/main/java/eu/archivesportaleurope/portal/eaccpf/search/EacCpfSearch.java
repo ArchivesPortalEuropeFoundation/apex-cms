133db912cdcdd4b3486367f9b5227ffafe441a67
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-02-10 12:44:29 +0000

    IssueID 833
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1238 8f6216c4-6850-4946-a993-29be1cf876c3

---
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/eaccpf/search/EacCpfSearch.java
@@ -0,0 +1,14 @@
+package eu.archivesportaleurope.portal.eaccpf.search;
+
+public class EacCpfSearch {
+	private String term;
+
+	public String getTerm() {
+		return term;
+	}
+
+	public void setTerm(String term) {
+		this.term = term;
+	}
+	
+}
