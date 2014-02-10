7102f599f4d067518b7a91afd1a4c4448ec22dfb
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-02-10 12:45:16 +0000

    IssueID 833
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1240 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/eaccpf/search/SearchEacCpfContoller.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/eaccpf/search/SearchEacCpfContoller.java
@@ -1,8 +1,14 @@
 package eu.archivesportaleurope.portal.eaccpf.search;
 
+import java.text.ParseException;
+
+import javax.portlet.RenderRequest;
+
 import org.apache.log4j.Logger;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
@@ -25,9 +31,22 @@ public class SearchEacCpfContoller {
 	}
 	@RenderMapping
 	public ModelAndView searchEacCpf() {
+		LOGGER.info("no");
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("index");
 		return modelAndView;
 	}
+	@RenderMapping(params = "myaction=eacCpfSearch")
+	public ModelAndView search(@ModelAttribute(value = "eacCpfSearch") EacCpfSearch eacCpfSearch,RenderRequest request) throws SolrServerException, ParseException {
+		LOGGER.info("yes");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		modelAndView.getModelMap().addAttribute("test", "yes");
+		return modelAndView;
+	}
 
+	@ModelAttribute("eacCpfSearch")
+	public EacCpfSearch getCommandObject() {
+		return new EacCpfSearch();
+	}
 }
