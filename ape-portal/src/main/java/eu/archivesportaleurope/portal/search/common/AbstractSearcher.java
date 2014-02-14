c8cdb110c5584e6e789c852cb171d7663da371e1
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-02-14 10:12:26 +0000

    IssueID 833: Add EAC-CPF to search
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1285 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/common/AbstractSearcher.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/common/AbstractSearcher.java
@@ -239,11 +239,10 @@ public abstract class AbstractSearcher {
 			query.set("spellcheck", "on");
 		}
 		long startTime = System.currentTimeMillis();
-		LOGGER.info("Query(" + queryType + "): " +getSolrSearchUrl() + "/select?"+ query.toString());
 		QueryResponse result =  getSolrServer().query(query, METHOD.POST);
-		if (true || LOGGER.isDebugEnabled()){
+		if (LOGGER.isDebugEnabled()){
 			long duration = System.currentTimeMillis() - startTime;
-			LOGGER.info("Query(" + queryType + ", hits: "+result.getResults().getNumFound()+ ", d: " +duration + "ms): " +getSolrSearchUrl() + "/select?"+ query.toString());
+			LOGGER.debug("Query(" + queryType + ", hits: "+result.getResults().getNumFound()+ ", d: " +duration + "ms): " +getSolrSearchUrl() + "/select?"+ query.toString());
 		}
 		return result;
 	}
