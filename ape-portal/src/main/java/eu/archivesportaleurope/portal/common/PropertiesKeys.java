afd122ccbd26367674d2011b6f0aa991fd2e6d0d
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-03-03 13:47:58 +0000

    IssueID 1035: Contact form
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.5@1345 8f6216c4-6850-4946-a993-29be1cf876c3

---
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/common/PropertiesKeys.java
@@ -0,0 +1,19 @@
+package eu.archivesportaleurope.portal.common;
+
+
+
+public class PropertiesKeys {
+
+	public static final String APE_EMAILS_FEEDBACK_TECHNICAL = "ape.emails.feedback.technical";
+	public static final String APE_EMAILS_CONTRIBUTE = "ape.emails.contribute";
+	public static final String APE_EMAILS_SUGGESTIONS = "ape.emails.suggestions";
+	public static final String APE_EMAILS_FEEDBACK_USER = "ape.emails.feedback.user";
+
+	public static final String LIFERAY_RECAPTCHA_PUBLIC_KEY = "captcha.engine.recaptcha.key.public";
+	public static final String LIFERAY_RECAPTCHA_PRIVATE_KEY = "captcha.engine.recaptcha.key.private";
+	public static final String LIFERAY_RECAPTCHA_URL_VERIFY = "captcha.engine.recaptcha.url.verify";
+	public static final String LIFERAY_RECAPTCHA_PUB_KEY = "captcha.engine.recaptcha.key.public";
+	public static final String LIFERAY_RECAPTCHA_URL = "captcha.engine.recaptcha.url.script";
+	public static final String APE_RECAPTCHA_URL = "ape.captcha.engine.recaptcha.url.script.ajax";
+
+}
