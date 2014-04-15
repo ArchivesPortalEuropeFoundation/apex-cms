bf97242e05f42fa3b1f29a34792ccb0bf7720eaa
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-04-15 07:28:49 +0000

    IssueID 835: Implement preview
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1540 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/common/jsp/GenerateSearchWordsTag.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/common/jsp/GenerateSearchWordsTag.java
@@ -8,13 +8,15 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
 import eu.apenet.commons.solr.HighlightType;
 import eu.apenet.commons.solr.HighlightUtil;
 import eu.apenet.commons.utils.APEnetUtilities;
+import eu.archivesportaleurope.portal.eaccpf.search.DisplayPreviewContoller;
 
 public class GenerateSearchWordsTag extends SimpleTagSupport {
-
+	private final static Logger LOGGER = Logger.getLogger(GenerateSearchWordsTag.class);
 	private String element;
 	private String term;
 	private String var;
@@ -22,10 +24,12 @@ public class GenerateSearchWordsTag extends SimpleTagSupport {
 	@Override
 	public void doTag() throws JspException, IOException {
 		String result = "";
+		LOGGER.info(term + " ------ " + element);
 		if (StringUtils.isNotBlank(term) && StringUtils.isNotBlank(element)) {
 			List<String> words = HighlightUtil.getInstance(APEnetUtilities.getApePortalConfig().getSolrStopwordsUrl())
 					.convertSearchTermToWords(term, HighlightType.getHighlightType(element));
 			for (int i = 0; i < words.size(); i++){
+				LOGGER.info("word: " + words.get(i));
 				if (i > 0){
 					result+="_" + words.get(i);
 				}else {
