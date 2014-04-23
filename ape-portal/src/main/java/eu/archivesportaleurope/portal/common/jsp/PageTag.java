bee91ff166ca427cfdc53e6ab0a3e7ad51622050
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-04-23 08:28:58 +0000

    IssueID 1158: simple search in more pages
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1579 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/common/jsp/PageTag.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/common/jsp/PageTag.java
@@ -1,6 +1,7 @@
 package eu.archivesportaleurope.portal.common.jsp;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.portlet.PortletRequest;
 import javax.servlet.jsp.JspException;
@@ -9,39 +10,70 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 import org.apache.log4j.Logger;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutTypePortlet;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 
+import eu.apenet.commons.utils.Cache;
+import eu.archivesportaleurope.util.ApeUtil;
+
 public class PageTag extends SimpleTagSupport {
+	private static final String SEPARATOR = ":";
 	private static final String _WAR = "_WAR";
 	private final static Logger LOGGER = Logger.getLogger(PageTag.class);
 	private String portletName;
 	private String friendlyUrl;
 	private String varPlId;
 	private String varPortletId; 
+	private final static Cache<String, String> CACHE = new Cache<String, String>();
 	@Override
 	public void doTag() throws JspException, IOException {
+		try {
+			String key = friendlyUrl + SEPARATOR + portletName;
+			String result = CACHE.get(key);
+			if (result == null) {
+				result = retrievePortletName(portletName, friendlyUrl);
+				CACHE.put(key, result);
+			}
+			String[] splitted = result.split(SEPARATOR);
+			getJspContext().setAttribute(varPlId, splitted[0]);
+			getJspContext().setAttribute(varPortletId, splitted[1]);
+			
+		} catch (Exception e) {
+			LOGGER.error("Unable to retrieve portletId and plId: " +ApeUtil.generateThrowableLog(e));
+		}
+
+		super.doTag();
+	}
+
+	public String retrievePortletName(String portletName, String friendlyUr){
 		PortletRequest portletRequest = (PortletRequest) ((PageContext) getJspContext()).getRequest().getAttribute(
 				"javax.portlet.request");
 		ThemeDisplay themeDisplay = (ThemeDisplay) portletRequest.getAttribute(WebKeys.THEME_DISPLAY);
-		String portletId = (String) portletRequest.getAttribute(WebKeys.PORTLET_ID);
-		int index = portletId.indexOf(_WAR);
-		String newPortletId = portletName + portletId.substring(index);
-		getJspContext().setAttribute(varPortletId, newPortletId);
 		long groupId = themeDisplay.getLayout().getGroupId();
 		Layout otherPage;
 		try {
 			otherPage = LayoutLocalServiceUtil.getFriendlyURLLayout(groupId, false, friendlyUrl);
-			getJspContext().setAttribute(varPlId, otherPage.getPlid());
+			String result = otherPage.getPlid() + SEPARATOR;
+			String newPortletId = portletName + _WAR;
+			LayoutTypePortlet layoutTypePortlet = (LayoutTypePortlet)otherPage.getLayoutType();
+			List<String> actualPortletList = layoutTypePortlet.getPortletIds();
+			boolean found = false;
+			for (int i=0; !found && i < actualPortletList.size();i++){
+				String portletId = actualPortletList.get(i);
+				if (portletId.startsWith(newPortletId)){
+					found = true;
+					result += portletId;
+					
+				}
+			}
+			return result;
 		} catch (Exception e) {
-			LOGGER.error("Unable to retrieve portletId and plId: " +e.getMessage(), e);
+			LOGGER.error("Unable to retrieve portletId and plId: " +ApeUtil.generateThrowableLog(e));
 		}
-
-		super.doTag();
+		return null;
 	}
 	public String getPortletName() {
 		return portletName;
