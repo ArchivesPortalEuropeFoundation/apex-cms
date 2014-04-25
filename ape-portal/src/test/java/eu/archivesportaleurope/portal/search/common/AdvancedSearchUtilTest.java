d128973ecf17a65368640cb62fd5a24a1d103f52
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-04-25 10:15:30 +0000

    refactoring
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1630 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/test/java/eu/archivesportaleurope/portal/search/common/AdvancedSearchUtilTest.java
+++ ape-portal/src/test/java/eu/archivesportaleurope/portal/search/common/AdvancedSearchUtilTest.java
@@ -10,16 +10,16 @@ public class AdvancedSearchUtilTest {
 	@Test
 	public void testIsValidDate() {
 		// start date
-		Assert.assertEquals("1988-12-30", AdvancedSearchUtil.parseDate("1988/12/30", true));
-		Assert.assertEquals("1988-12-01", AdvancedSearchUtil.parseDate("1988/12", true));
-		Assert.assertEquals("1988-01-01", AdvancedSearchUtil.parseDate("1988", true));
+		Assert.assertEquals("1988-12-30", SearchUtil.parseDate("1988/12/30", true));
+		Assert.assertEquals("1988-12-01", SearchUtil.parseDate("1988/12", true));
+		Assert.assertEquals("1988-01-01", SearchUtil.parseDate("1988", true));
 		// enddate
-		Assert.assertEquals("1988-10-30", AdvancedSearchUtil.parseDate("1988/10/30", false));
-		Assert.assertEquals("1988-10-31", AdvancedSearchUtil.parseDate("1988/10", false));
-		Assert.assertEquals("1988-12-31", AdvancedSearchUtil.parseDate("1988", false));
+		Assert.assertEquals("1988-10-30", SearchUtil.parseDate("1988/10/30", false));
+		Assert.assertEquals("1988-10-31", SearchUtil.parseDate("1988/10", false));
+		Assert.assertEquals("1988-12-31", SearchUtil.parseDate("1988", false));
 	
-		Assert.assertEquals(null, AdvancedSearchUtil.parseDate("1988/30/30", true));
-		Assert.assertEquals(null, AdvancedSearchUtil.parseDate("11-12-1988", true));
+		Assert.assertEquals(null, SearchUtil.parseDate("1988/30/30", true));
+		Assert.assertEquals(null, SearchUtil.parseDate("11-12-1988", true));
 		
 	}
 
