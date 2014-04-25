a83c7ee4cd46a3cf11c6379a21b438d2d68b3a9a
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-04-25 05:45:35 +0000

    IssueID 1160: Caching
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1621 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/simple/SimpleSearchController.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/simple/SimpleSearchController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
+import eu.apenet.commons.utils.Cache;
 import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
 import eu.apenet.persistence.dao.EadDAO;
 import eu.archivesportaleurope.portal.common.PortalDisplayUtil;
@@ -18,11 +19,16 @@ import eu.archivesportaleurope.portal.common.PortalDisplayUtil;
 @Controller(value = "simpleSearchController")
 @RequestMapping(value = "VIEW")
 public class SimpleSearchController {
+	private static final String INSTITUTIONS = "institutions";
+	private static final String UNITS = "units";
 	private static final String TRUE = "TRUE";
 	//private final static Logger LOGGER = Logger.getLogger(SimpleSearchController.class);
 	private ArchivalInstitutionDAO archivalInstitutionDAO;
 	private EadDAO eadDAO;
 
+	private final static Cache<String, Long> CACHE = new Cache<String, Long>();
+	
+	
 	@RenderMapping
 	public ModelAndView showSimpleSearch(RenderRequest request) {
 		ModelAndView modelAndView = new ModelAndView();
@@ -31,10 +37,19 @@ public class SimpleSearchController {
 			modelAndView.setViewName("embedded");
 		}else {
 			modelAndView.setViewName("home");
-	
-			modelAndView.getModelMap().addAttribute("institutions", archivalInstitutionDAO.countArchivalInstitutionsWithContentIndexed());
+			Long institutions = CACHE.get(INSTITUTIONS);
+			Long units = CACHE.get(UNITS);
+			if (institutions == null) {
+				institutions = archivalInstitutionDAO.countArchivalInstitutionsWithContentIndexed();
+				CACHE.put(INSTITUTIONS, institutions);
+			}	
+			if (units == null) {
+				units = eadDAO.getTotalCountOfUnits();
+				CACHE.put(UNITS, units);
+			}	
+			modelAndView.getModelMap().addAttribute(INSTITUTIONS, institutions);
 			NumberFormat numberFormat = NumberFormat.getInstance(request.getLocale());
-			modelAndView.getModelMap().addAttribute("units", numberFormat.format(eadDAO.getTotalCountOfUnits()));
+			modelAndView.getModelMap().addAttribute(UNITS, numberFormat.format(units));
 			String numberOfDaoUnitsString= request.getPreferences().getValue("numberOfDaoUnits", "0");
 			long numberOfDaoUnits = 0;
 			if (StringUtils.isNotBlank(numberOfDaoUnitsString)){
