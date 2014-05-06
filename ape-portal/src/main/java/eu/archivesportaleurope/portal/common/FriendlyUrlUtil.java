036d541adc75ce14b41afe316cc138476ab9e139
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-05-06 11:51:07 +0000

    IssueID 1174: Persistent urls
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1705 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/common/FriendlyUrlUtil.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/common/FriendlyUrlUtil.java
@@ -16,12 +16,8 @@ import eu.archivesportaleurope.portal.common.urls.SitemapUrl;
 
 public final class FriendlyUrlUtil {
 	private final static Logger LOGGER = Logger.getLogger(FriendlyUrlUtil.class);
-	public static final String OLD_EAD_DISPLAY_SEARCH = "eaddisplay-search";
-	public static final String OLD_EAD_DISPLAY_FRONTPAGE = "eaddisplay-frontpage";
 	public static final String EAD_DISPLAY_PERSISTENT = "eaddisplay-persistent-archdesc";	
 	public static final String EAD_DISPLAY_PERSISTENT_PAGING = "eaddisplay-persistent-paging";		
-	public static final String OLD_EAD_DISPLAY_SEARCH_PAGING = "eaddisplay-search-paging";
-	public static final String OLD_EAD_DISPLAY_FRONTPAGE_PAGING = "eaddisplay-frontpage-paging";
 	public static final String FEATURED_EXHIBITION = "featured-exhibition-details";
 	public static final String FEATURED_EXHIBITION_ARTICLE = "featured-exhibition-details-article";
 	public static final String DIRECTORY_COUNTRY = "directory-country";
@@ -40,8 +36,6 @@ public final class FriendlyUrlUtil {
 	
 	private final static Map<String, String> urls = new HashMap<String, String>();
 	static {
-		urls.put(OLD_EAD_DISPLAY_SEARCH, "/ead-display/-/ead/s");
-		urls.put(OLD_EAD_DISPLAY_FRONTPAGE, "/ead-display/-/ead/fp");
 		urls.put(EAD_DISPLAY_PERSISTENT, "/ead-display/-/ead/pl");
 		urls.put(FEATURED_EXHIBITION, "/featured-document/-/fed/pk");
 		urls.put(FEATURED_EXHIBITION_ARTICLE, "/featured-document/-/fed/a");
@@ -49,8 +43,6 @@ public final class FriendlyUrlUtil {
 		urls.put(DIRECTORY_INSTITUTION_ID, "/directory/-/dir/ai/id");
 		urls.put(DIRECTORY_INSTITUTION_CODE, "/directory/-/dir/ai/code");
 		urls.put(DIRECTORY_CONTENT, "/directory/-/dir/content");
-		urls.put(OLD_EAD_DISPLAY_SEARCH_PAGING, "/ead-display/-/ead/s-p");
-		urls.put(OLD_EAD_DISPLAY_FRONTPAGE_PAGING, "/ead-display/-/ead/fp-p");
 		urls.put(DIRECTORY_SITEMAP, "/directory/-/dir/sitemap");
 		urls.put(SEARCH, "/search");
 		urls.put(SAVED_SEARCH, "/search/-/s/d");
