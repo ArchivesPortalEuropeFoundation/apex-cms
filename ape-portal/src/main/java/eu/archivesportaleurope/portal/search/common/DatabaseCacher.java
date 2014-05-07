b5773ba4bc6a308d18f1b679a18277ce3e3d7575
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-05-07 06:40:50 +0000

    IssueID 1174: Persistent urls
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1720 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/common/DatabaseCacher.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/common/DatabaseCacher.java
@@ -1,15 +1,22 @@
 package eu.archivesportaleurope.portal.search.common;
 
+import eu.apenet.commons.utils.Cache;
 import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
 import eu.apenet.persistence.vo.ArchivalInstitution;
 
 public class DatabaseCacher {
 
 	private ArchivalInstitutionDAO archivalInstitutionDAO;
-	
+	private final Cache<Integer, String> repositoryCodeCache = new Cache<Integer, String>();
+
 	public String getRepositoryCode(Integer aiId){
-		ArchivalInstitution archivalInstitution = archivalInstitutionDAO.getArchivalInstitution(aiId);
-		return archivalInstitution.getRepositorycode();
+		String repositoryCode = repositoryCodeCache.get(aiId);
+		if (repositoryCode == null){
+			ArchivalInstitution archivalInstitution = archivalInstitutionDAO.getArchivalInstitution(aiId);
+			repositoryCode = archivalInstitution.getRepositorycode();
+			repositoryCodeCache.put(aiId, repositoryCode);
+		}
+		return repositoryCode;
 	}
 
 	public void setArchivalInstitutionDAO(ArchivalInstitutionDAO archivalInstitutionDAO) {
