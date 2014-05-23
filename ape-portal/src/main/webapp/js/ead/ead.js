cd6c80a009aff140217337076a54561d8b1c9401
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-05-23 12:55:12 +0000

    IssueID 1236: feedback
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.5@1788 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/webapp/js/ead/ead.js
+++ ape-portal/src/main/webapp/js/ead/ead.js
@@ -189,8 +189,17 @@ function showFeedback(feedbackUrl, aiId, documentTitle, documentUrl, publicKey) 
 }
 function sendFeedback(){
 	var url = $("#contactForm").attr("action");
+	var publicKey = $("#contactForm #recaptchaPubKey").attr("value");
 	$.post(url, $("#contactForm").serialize(), function(data) {
 		$("#feedbackContent").html(data);
+        Recaptcha.create(publicKey, "recaptchaDiv", {
+            theme: "white",
+            callback: Recaptcha.focus_response_field});
+        $("#feedbackContent").removeClass("feedbackContent").addClass("feedbackContent");
+    	$("#contactFeedbackSend").click(function(event) {
+    		event.preventDefault();
+    		sendFeedback();
+    	});		
 	});
 	var aiName = $("#contactForm #aiName").html();	
 	var aiRepoCode = $("#contactForm #aiRepoCode").attr("value");
