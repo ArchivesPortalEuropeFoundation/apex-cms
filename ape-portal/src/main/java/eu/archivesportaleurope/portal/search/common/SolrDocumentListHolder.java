07d9ca343fc71f43d6af3809dcf76d3eaddfd575
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-06-19 12:40:14 +0000

    IssueID 669: EAG search
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@1912 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/common/SolrDocumentListHolder.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/common/SolrDocumentListHolder.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
 
+import eu.archivesportaleurope.portal.common.SpringResourceBundleSource;
 import eu.archivesportaleurope.portal.search.eaccpf.EacCpfSearchResult;
 import eu.archivesportaleurope.portal.search.ead.list.EadSearchResult;
 import eu.archivesportaleurope.portal.search.eag.EagSearchResult;
@@ -17,25 +18,28 @@ public class SolrDocumentListHolder implements Iterable<SearchResult> {
 	private Map<String, Map<String, List<String>>> highlightingMap;
 	private Class<? extends SearchResult> searchResultClass;
 	private DatabaseCacher databaseCacher;
+	private SpringResourceBundleSource resourceBundleSource;
 	
 	public SolrDocumentListHolder(){
 		this.list = null;
 		this.highlightingMap = null;
 	}
-	public SolrDocumentListHolder(QueryResponse response, Class<? extends SearchResult> searchResultClass){
+	public SolrDocumentListHolder(QueryResponse response, Class<? extends SearchResult> searchResultClass, SpringResourceBundleSource resourceBundleSource){
 		this.list = response.getResults();
 		this.highlightingMap = response.getHighlighting();
 		this.searchResultClass = searchResultClass;
+		this.resourceBundleSource = resourceBundleSource;
 	}
-	public SolrDocumentListHolder(QueryResponse response, Class<? extends SearchResult> searchResultClass, DatabaseCacher databaseCacher){
+	public SolrDocumentListHolder(QueryResponse response, Class<? extends SearchResult> searchResultClass,SpringResourceBundleSource resourceBundleSource, DatabaseCacher databaseCacher){
 		this.list = response.getResults();
 		this.highlightingMap = response.getHighlighting();
 		this.searchResultClass = searchResultClass;
 		this.databaseCacher = databaseCacher;
+		this.resourceBundleSource = resourceBundleSource;
 	}
 	@Override
 	public Iterator<SearchResult> iterator() {
-		return new SolrDocumentListIterator(list, highlightingMap, searchResultClass,databaseCacher);
+		return new SolrDocumentListIterator(list, highlightingMap, searchResultClass,databaseCacher, resourceBundleSource);
 	}
 
 	public static class SolrDocumentListIterator implements Iterator<SearchResult>{
@@ -45,11 +49,13 @@ public class SolrDocumentListHolder implements Iterable<SearchResult> {
 		private Map<String, Map<String, List<String>>> highlightingMap;
 		private DatabaseCacher databaseCacher;
 		private Class<? extends SearchResult> searchResultClass;
-		public SolrDocumentListIterator(SolrDocumentList list, Map<String, Map<String, List<String>>> highlightingMap, Class<? extends SearchResult> searchResultClass, DatabaseCacher databaseCacher){
+		private  SpringResourceBundleSource resourceBundleSource;
+		public SolrDocumentListIterator(SolrDocumentList list, Map<String, Map<String, List<String>>> highlightingMap, Class<? extends SearchResult> searchResultClass, DatabaseCacher databaseCacher,  SpringResourceBundleSource resourceBundleSource){
 			this.list = list;
 			this.highlightingMap = highlightingMap;
 			this.searchResultClass = searchResultClass;
 			this.databaseCacher = databaseCacher;
+			this.resourceBundleSource = resourceBundleSource;
 		}		
 		@Override
 		public boolean hasNext() {
@@ -70,7 +76,7 @@ public class SolrDocumentListHolder implements Iterable<SearchResult> {
 			}else if (EacCpfSearchResult.class.equals(searchResultClass)){
 				item =  new EacCpfSearchResult(list.get(currentPosition), highlightingMap);
 			}else if (EagSearchResult.class.equals(searchResultClass)){
-				item =  new EagSearchResult(list.get(currentPosition), highlightingMap);
+				item =  new EagSearchResult(list.get(currentPosition), highlightingMap, resourceBundleSource);
 			}
 			currentPosition++;
 			return item;
