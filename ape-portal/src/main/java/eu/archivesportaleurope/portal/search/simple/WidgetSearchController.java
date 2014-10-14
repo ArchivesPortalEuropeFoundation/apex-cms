9474bb1b3dc3da2fd28362ece4e802ea42e043f0
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-14 10:27:37 +0000

    improve widget
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@2550 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/simple/WidgetSearchController.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/simple/WidgetSearchController.java
@@ -1,5 +1,7 @@
 package eu.archivesportaleurope.portal.search.simple;
 
+import java.util.regex.Pattern;
+
 import javax.portlet.RenderRequest;
 
 import org.apache.commons.lang.StringUtils;
@@ -12,20 +14,40 @@ import org.springframework.web.portlet.bind.annotation.RenderMapping;
 @Controller(value = "widgetSearchController")
 @RequestMapping(value = "VIEW")
 public class WidgetSearchController {
-
+	private final static Pattern COLOR_PATTERN = Pattern.compile("[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}");
 	@RenderMapping
 	public ModelAndView showSimpleSearch(RenderRequest request) {
 		ModelAndView modelAndView = new ModelAndView();
 		String savedSearchId = request.getParameter("savedSearchId");
-		if (StringUtils.isNotBlank(savedSearchId) && StringUtils.isNumeric(savedSearchId)){
+		updateColors(modelAndView, request);
+		if (StringUtils.isNotBlank(savedSearchId) && StringUtils.isNumeric(savedSearchId)) {
 			modelAndView.getModel().put("savedSearchId", savedSearchId);
 			modelAndView.setViewName("widget");
-		}else {
+		} else {
 			modelAndView.setViewName("widget-all");
 		}
-		
 
 		return modelAndView;
+	}
+	
+	private void updateColors(ModelAndView modelAndView, RenderRequest request){
+		String backgroundColor = request.getParameter("backgroundColor");
+		String buttonBackgroundColor = request.getParameter("buttonBackgroundColor");
+		String buttonBorderColor = request.getParameter("buttonBorderColor");
+		String buttonTextColor = request.getParameter("buttonTextColor");
+		modelAndView.getModel().put("backgroundColor", getCss("background-color", backgroundColor));
+		String buttonStyle = "";
+		buttonStyle += getCss("background-color", buttonBackgroundColor);
+		buttonStyle += getCss("color", buttonTextColor);	
+		buttonStyle += getCss("border-color", buttonBorderColor);	
+		modelAndView.getModel().put("buttonStyle", buttonStyle);
+	}
+	private String getCss(String name, String value){
+		if (StringUtils.isNotBlank(value) && COLOR_PATTERN.matcher(value).matches()){
+			return name+ ": #" +value +";";
+		}else {
+			return "";
+		}		
 	}
 
 	@ModelAttribute("simpleSearch")
