9cc0406345e8d6d2388b7e3e6a5355b8429e6c2c
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-11-07 09:01:05 +0000

    IssueID 1277 unitid
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/branches/frontend-project-1.7@2703 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/search/ead/list/EadSearchResult.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/search/ead/list/EadSearchResult.java
@@ -153,12 +153,10 @@ public class EadSearchResult extends SearchResult{
 		// Check if the content has char '<' or '>' in order to avoid
 		// js ingestion (see issue 1248).
 		Object object = solrDocument.getFieldValue(SolrFields.UNITID_OF_FOND);
-		if (object instanceof String) {
-			String unitIdFond = (String) object;
-			unitIdFond = unitIdFond.replaceAll(">", "&#62;").replaceAll("<","&#60;");
-			return unitIdFond;
+		if (object != null && object instanceof String) {
+			return DisplayUtils.encodeHtml(object.toString());
 		} else {
-			return solrDocument.getFieldValue(SolrFields.UNITID_OF_FOND);
+			return null;
 		}
 	}
 	
