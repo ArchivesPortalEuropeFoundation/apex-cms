ce1e19daf702a8ae0a80e08f1b5a303bec709f53
julio <julio@archivesportaleurope.net>
2014-12-12 13:23:09 +0000

    Bug #1757: Bookmarks: Add to collections. - Reupload code deleted in a previous commit.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@2905 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/collections/CollectionContentController.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/collections/CollectionContentController.java
@@ -221,7 +221,7 @@ public class CollectionContentController {
 				//recover selected collections from the request
 				List<Long> collectionsList = new ArrayList<Long>();
 				String[] listChecked = {""};
-				if (request.getParameter("listChecked") == null) {
+				if (request.getParameter("listChecked") == null || request.getParameter("listChecked").isEmpty()) {
 					listChecked = null;
 				} else {
 					listChecked = request.getParameter("listChecked").split(",");
@@ -274,7 +274,20 @@ public class CollectionContentController {
 						modelAndView.getModelMap().addAttribute("collections",collectionsWithBookmark);	
 						return modelAndView;
 					}
-				} 
+				} else if (listChecked == null) {
+					modelAndView = showAddSavedElement(request, savedSearch, bookmark, type);
+					modelAndView.getModelMap().addAttribute("isNoCollectionsSelected", true);
+
+					if (BOOKMARK.equalsIgnoreCase(type)) {
+						bookmark.setId(Long.toString(elementId));
+						modelAndView.getModelMap().addAttribute("savedBookmark", bookmark);
+					} else {
+						savedSearch.setId(Long.toString(elementId));
+						modelAndView.getModelMap().addAttribute("savedSearch", savedSearch);
+					}
+
+					return modelAndView;
+				}
 			} catch (Exception e) {
 				LOGGER.error(ApeUtil.generateThrowableLog(e));
 				modelAndView.getModelMap().addAttribute("loggedIn", false);
