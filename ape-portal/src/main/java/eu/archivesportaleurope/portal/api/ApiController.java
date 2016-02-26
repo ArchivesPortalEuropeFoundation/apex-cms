c4160ee2a369fd6a15b98079c6f8539e2074c5e4
smartKaisar <kaisar39@gmail.com>
2016-02-26 07:17:34 +0000

    Add widget for api key
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-cms/trunk@3373 8f6216c4-6850-4946-a993-29be1cf876c3

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/api/ApiController.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/api/ApiController.java
@@ -66,108 +66,28 @@ public class ApiController {
 
     @ActionMapping(params = "myaction=getApiKey")
     public void saveApiKey(@ModelAttribute("apiKey") ApiKey apiKey, ActionRequest actionRequest, ActionResponse response) throws IOException {
+        System.out.println(":::" + apiKey.getKey() + ":" + this.apiKey.getKey() + ":::");
+        if ("renew".equals(apiKey.getKey())) {
+            System.out.println(":::Change Request:::");
+        } else {
+            System.out.println(":::New Request:::");
+        }
+        apiKey.setKey("changed");
+        this.apiKey = apiKey;
+        System.out.println(":::" + apiKey.getKey() + ":" + this.apiKey.getKey() + ":::");
+        response.sendRedirect("/api-key", null);
+    }
+
+    @ActionMapping(params = "myaction=changeApiKey")
+    public void changeApiKeyView(@ModelAttribute("apiKey") ApiKey apiKey, ActionRequest actionRequest, ActionResponse response) throws IOException {
+        apiKey.setKey("renew");
+        this.apiKey = apiKey;
+    }
+
+    @ActionMapping(params = "myaction=doChangeApiKey")
+    public void changeApiKey(@ModelAttribute("apiKey") ApiKey apiKey, ActionRequest actionRequest, ActionResponse response) throws IOException {
         apiKey.setKey("changed");
         this.apiKey = apiKey;
         response.sendRedirect("/api-key");
     }
-
-    @ActionMapping(params = "myaction=cleanServer")
-    public void cleanServer(ActionRequest actionRequest, ActionResponse response) {
-        try {
-            ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
-            long groupId = themeDisplay.getLayout().getGroupId();
-            List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(groupId, false);
-            for (Layout layout : layouts) {
-                layout.setThemeId(null);
-                //layout.getExpandoBridge().setAttribute("display-in-footer", null);
-                LayoutLocalServiceUtil.updateLayout(layout);
-            }
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-    }
-
-    @ActionMapping(params = "myaction=cleanTranslations")
-    public void cleanTranslations(ActionRequest actionRequest, ActionResponse response) {
-        try {
-            ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
-            long groupId = themeDisplay.getLayout().getGroupId();
-            List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(groupId, false);
-            Locale defaultLocale = themeDisplay.getCompany().getLocale();
-            LOGGER.info("Default locale: " + defaultLocale);
-            for (Layout layout : layouts) {
-                String url = layout.getFriendlyURL().substring(1);
-                Map<Locale, String> nameMap = layout.getNameMap();
-                for (Locale locale : nameMap.keySet()) {
-                    if (!locale.equals(defaultLocale)) {
-                        LOGGER.info("Page " + url + " remove translation " + locale);
-                        layout.setName(null, locale);
-                        layout.setTitle(null, locale);
-
-                    }
-                }
-                LayoutLocalServiceUtil.updateLayout(layout);
-            }
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-    }
-
-    @ActionMapping(params = "myaction=updateTranslations")
-    public void updateTranslations(ActionRequest actionRequest, ActionResponse response) {
-        try {
-            ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
-            long groupId = themeDisplay.getLayout().getGroupId();
-            List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(groupId, false);
-            Locale defaultLocale = themeDisplay.getCompany().getLocale();
-            Locale.setDefault(defaultLocale);
-            LOGGER.info("Default locale: " + defaultLocale);
-            for (Layout layout : layouts) {
-                String url = layout.getFriendlyURL().substring(1);
-                String resourcePropertyMenuName = "menu." + url;
-                String resourcePropertyMenuTitle = resourcePropertyMenuName + ".title";
-                Locale[] locales = LanguageUtil.getAvailableLocales();
-                String defaultLocaleMenuName = "";
-
-                try {
-                    defaultLocaleMenuName = messageSource.getMessage(resourcePropertyMenuName, null, defaultLocale);
-                    LOGGER.info("Page " + url + " add default " + defaultLocale + " " + defaultLocaleMenuName);
-                    layout.setName(defaultLocaleMenuName, defaultLocale);
-                } catch (NoSuchMessageException e) {
-                    LOGGER.error("No translation " + defaultLocale + ": " + resourcePropertyMenuName);
-                }
-                String defaultLocaleMenuTitle = "";
-                try {
-                    defaultLocaleMenuTitle = messageSource.getMessage(resourcePropertyMenuTitle, null, defaultLocale);
-                    LOGGER.info("Page " + url + " add default " + defaultLocale + " " + defaultLocaleMenuTitle);
-                    layout.setTitle(defaultLocaleMenuTitle, defaultLocale);
-                } catch (NoSuchMessageException e) {
-                    LOGGER.error("No translation " + defaultLocale + ": " + resourcePropertyMenuTitle);
-                }
-                for (Locale locale : locales) {
-                    try {
-                        String localizedMenuName = messageSource.getMessage(resourcePropertyMenuName, null, locale);
-                        if (StringUtils.isNotBlank(localizedMenuName) && !localizedMenuName.equals(defaultLocaleMenuName)) {
-                            LOGGER.info("Page " + url + " add translation " + locale + " " + localizedMenuName);
-                            layout.setName(localizedMenuName, locale);
-                        }
-                    } catch (NoSuchMessageException e) {
-                    }
-
-                    try {
-                        String localizedMenuTitle = messageSource.getMessage(resourcePropertyMenuTitle, null, locale);
-                        if (StringUtils.isNotBlank(localizedMenuTitle) && !localizedMenuTitle.equals(defaultLocaleMenuName)) {
-                            LOGGER.info("Page " + url + " add translation " + locale + " " + localizedMenuTitle);
-                            layout.setTitle(localizedMenuTitle, locale);
-                        }
-                    } catch (NoSuchMessageException e) {
-                    }
-                }
-                LayoutLocalServiceUtil.updateLayout(layout);
-            }
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-    }
-
 }
--- ape-portal/src/main/webapp/WEB-INF/jsp/api/index.jsp
+++ ape-portal/src/main/webapp/WEB-INF/jsp/api/index.jsp
@@ -10,17 +10,20 @@
 <%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui"%>
 <%@ taglib prefix="liferay-portlet" uri="http://liferay.com/tld/portlet"%>
 <portlet:defineObjects />
-
-<portlet:actionURL var="getApiKey">
+<portlet:actionURL var="actionUrl">
     <portlet:param name="myaction" value="getApiKey" />
 </portlet:actionURL>
+<portlet:actionURL var="changeApiKey">
+    <portlet:param name="myaction" value="changeApiKey" />
+</portlet:actionURL>
+
 
 <div id="languagePortlet">
     <h1>Get API Key</h1>
     <h2>API Key</h2>
 
     <form:form id="getApiKey" name="getApiKey" commandName="apiKey" method="post"
-               action="${getApiKey}">
+               action="${actionUrl}">
         <form:hidden path="firstName" value="${apiKey.firstName}"/>
         <form:hidden path="lastName" value="${apiKey.lastName}"/>
         <form:hidden path="email" value="${apiKey.email}"/>
@@ -39,7 +42,19 @@
                 <td><c:out value="${apiKey.email}"/></td>
             </tr>
             <c:choose>
-                <c:when test="${apiKey.key eq 'empty'}">
+                <c:when test="${apiKey.key ne 'empty' and apiKey.key ne 'renew'}">
+                    <tr>
+                        <th>Domain Name<!--<fmt:message key="savedsearch.id"/>-->:</th>
+                        <td><c:out value="${apiKey.domain}"/></td>
+                    </tr>
+                    <tr>
+                        <th>Your Api Key<!--<fmt:message key="savedsearch.id"/>-->:</th>
+                        <td><c:out value="${apiKey.key}"/></td>
+                    </tr>
+                    <a href="${changeApiKey}">Change Api Key</a>
+
+                </c:when>
+                <c:otherwise>
                     <tr>
                         <th class="description">Domain Name<!--<fmt:message key="savedsearch.id"/>-->:</th>
                         <td><form:input path="domain"  cssClass="longInput" maxlength="100" value="${apiKey.domain}"/></td>
@@ -49,16 +64,11 @@
                             <input type="submit" value="Submit"/>
                         </td>
                     </tr>
-                </c:when>
-                <c:otherwise>
-                    <tr>
-                        <th>Domain Name<!--<fmt:message key="savedsearch.id"/>-->:</th>
-                        <td><c:out value="${apiKey.domain}"/></td>
-                    </tr>
                     <tr>
                         <th>Your Api Key<!--<fmt:message key="savedsearch.id"/>-->:</th>
                         <td><c:out value="${apiKey.key}"/></td>
                     </tr>
+
                 </c:otherwise>
             </c:choose>
         </table>
