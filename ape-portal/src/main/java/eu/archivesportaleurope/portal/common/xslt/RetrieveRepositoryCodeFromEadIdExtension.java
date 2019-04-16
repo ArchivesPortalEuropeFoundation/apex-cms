4b0fefed0b77f2cf43eedbefba3795e1bd4ca221
stefan <stefan.papp.ape@gmail.com>
2019-04-16 15:07:21 +0200

    ADEV-217

--- ape-portal/src/main/java/eu/archivesportaleurope/portal/common/xslt/RetrieveRepositoryCodeFromEadIdExtension.java
+++ ape-portal/src/main/java/eu/archivesportaleurope/portal/common/xslt/RetrieveRepositoryCodeFromEadIdExtension.java
@@ -1,8 +1,10 @@
 package eu.archivesportaleurope.portal.common.xslt;
 
+import eu.apenet.persistence.dao.Ead3DAO;
 import eu.apenet.persistence.dao.EadDAO;
 import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.vo.Ead;
+import eu.apenet.persistence.vo.Ead3;
 import eu.apenet.persistence.vo.FindingAid;
 import eu.apenet.persistence.vo.HoldingsGuide;
 import eu.apenet.persistence.vo.SourceGuide;
@@ -11,10 +13,8 @@ import net.sf.saxon.expr.XPathContext;
 import net.sf.saxon.lib.ExtensionFunctionCall;
 import net.sf.saxon.lib.ExtensionFunctionDefinition;
 import net.sf.saxon.om.Sequence;
-import net.sf.saxon.om.SequenceIterator;
 import net.sf.saxon.om.StructuredQName;
 import net.sf.saxon.trans.XPathException;
-import net.sf.saxon.tree.iter.SingletonIterator;
 import net.sf.saxon.value.SequenceType;
 import net.sf.saxon.value.StringValue;
 
@@ -23,132 +23,210 @@ import net.sf.saxon.value.StringValue;
  * identifier of an apeEAD.
  */
 public class RetrieveRepositoryCodeFromEadIdExtension extends ExtensionFunctionDefinition {
-	/**
-	 * Serializable.
-	 */
-	private static final long serialVersionUID = -5996799246531865681L;
 
-	/**
-	 * Name of the function to call.
-	 */
-	private static final StructuredQName funcname = new StructuredQName("ape", "http://www.archivesportaleurope.eu/xslt/extensions",
-			"aiFromEad");
+    /**
+     * Serializable.
+     */
+    private static final long serialVersionUID = -5996799246531865681L;
 
-	private RetrieveRepositoryCodeFromEadIdCall retrieveRepositoryCodeFromEadIdCall;
+    /**
+     * Name of the function to call.
+     */
+    private static final StructuredQName funcname = new StructuredQName("ape", "http://www.archivesportaleurope.eu/xslt/extensions",
+            "aiFromEad");
 
-	public RetrieveRepositoryCodeFromEadIdExtension() {
-		this.retrieveRepositoryCodeFromEadIdCall = new RetrieveRepositoryCodeFromEadIdCall();
-	}
+    private RetrieveRepositoryCodeFromEadIdCall retrieveRepositoryCodeFromEadIdCall;
 
-	@Override
-	public SequenceType[] getArgumentTypes() {
-		return new SequenceType[] { SequenceType.OPTIONAL_STRING };
-	}
+    public RetrieveRepositoryCodeFromEadIdExtension() {
+        this.retrieveRepositoryCodeFromEadIdCall = new RetrieveRepositoryCodeFromEadIdCall();
+    }
 
-	@Override
-	public StructuredQName getFunctionQName() {
-		return RetrieveRepositoryCodeFromEadIdExtension.funcname;
-	}
+    @Override
+    public SequenceType[] getArgumentTypes() {
+        return new SequenceType[]{SequenceType.OPTIONAL_STRING};
+    }
 
-	@Override
-	public SequenceType getResultType(SequenceType[] arg0) {
-		return SequenceType.OPTIONAL_STRING;
-	}
+    @Override
+    public StructuredQName getFunctionQName() {
+        return RetrieveRepositoryCodeFromEadIdExtension.funcname;
+    }
 
-	@Override
-	public ExtensionFunctionCall makeCallExpression() {
-		return this.retrieveRepositoryCodeFromEadIdCall;
-	}
+    @Override
+    public SequenceType getResultType(SequenceType[] arg0) {
+        return SequenceType.OPTIONAL_STRING;
+    }
 
-	public int getMinimumNumberOfArguments() {
-		return 2;
-	}
+    @Override
+    public ExtensionFunctionCall makeCallExpression() {
+        return this.retrieveRepositoryCodeFromEadIdCall;
+    }
 
-	public int getMaximumNumberOfArguments() {
-		return 2;
-	}
+    public int getMinimumNumberOfArguments() {
+        return 2;
+    }
 
-	class RetrieveRepositoryCodeFromEadIdCall extends ExtensionFunctionCall {
-		/**
-		 * Serializable.
-		 */
-		private static final long serialVersionUID = 864847688629772008L;
+    public int getMaximumNumberOfArguments() {
+        return 2;
+    }
 
-		/**
-		 * Constructor.
-		 */
-		public RetrieveRepositoryCodeFromEadIdCall() {
-			super();
-		}
+    class RetrieveRepositoryCodeFromEadIdCall extends ExtensionFunctionCall {
 
-		@Override
+        /**
+         * Serializable.
+         */
+        private static final long serialVersionUID = 864847688629772008L;
+
+        /**
+         * Constructor.
+         */
+        public RetrieveRepositoryCodeFromEadIdCall() {
+            super();
+        }
+
+        @Override
         public Sequence call(XPathContext xPathContext, Sequence[] sequences) throws XPathException {
-			if (sequences!= null && sequences.length == 2) {
-				String firstArgValue = sequences[0].head().getStringValue();
-				String secondArgValue = sequences[1].head().getStringValue();
-				String value = "";
-				String type = "";
+            if (sequences != null && sequences.length == 2) {
+                String firstArgValue = sequences[0].head().getStringValue();
+                String secondArgValue = sequences[1].head().getStringValue();
+                String value = "";
+                String type = "";
 
-				// apeEAD.
-				EadDAO eadDAO = DAOFactory.instance().getEadDAO();
-				Ead ead = null;
+                // apeEAD and EAD3 lookup.
+                EadDAO eadDAO = DAOFactory.instance().getEadDAO();
+                Ead ead = null;
+                Ead3DAO ead3DAO = DAOFactory.instance().getEad3DAO();
+                Ead3 ead3 = null;
 
-				if (firstArgValue != null && !firstArgValue.isEmpty()) {
-					// Checks if exists a FA with the provided identifier.
-					if (secondArgValue != null && !secondArgValue.isEmpty()) {
-						// And repository code.
-						ead = eadDAO.getEadByEadid(FindingAid.class, secondArgValue, firstArgValue, true);
-						type = "fa";
-					} else {
-						// First FA with identifier.
-						ead = eadDAO.getFirstPublishedEadByEadid(FindingAid.class, firstArgValue);
-						type = "fa";
-					}
+                //TODO Eliminate duplicate code!
+                if (firstArgValue != null && !firstArgValue.isEmpty()) {
+                    if (firstArgValue.startsWith("ead3:")) {
+                        String identifier = firstArgValue.substring(5);
+                        // Checks if a EAD3 with the provided identifier exists.
+                        if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                            // And repository code.
+                            ead3 = ead3DAO.getEad3ByIdentifier(secondArgValue, identifier, true);
+                            type = "ead3";
+                        } else {
+                            // First FA with identifier.
+                            ead3 = ead3DAO.getFirstPublishedEad3ByIdentifier(identifier, true);
+                            type = "ead3";
+                        }
+                    } else if (firstArgValue.startsWith("ead2:")) {
+                        String eadid = firstArgValue.substring(5);
+                        if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                            // And repository code.
+                            ead = eadDAO.getEadByEadid(FindingAid.class, secondArgValue, eadid, true);
+                            type = "fa";
+                        } else {
+                            // First FA with identifier.
+                            ead = eadDAO.getFirstPublishedEadByEadid(FindingAid.class, eadid);
+                            type = "fa";
+                        }
 
-					if (ead == null) {
-						// Checks if exists a HG with the provided identifier.
-						if (secondArgValue != null && !secondArgValue.isEmpty()) {
-							// And repository code.
-							ead = eadDAO.getEadByEadid(HoldingsGuide.class, secondArgValue, firstArgValue, true);
-							type = "hg";
-						} else {
-							// First HG with identifier.
-							ead = eadDAO.getFirstPublishedEadByEadid(HoldingsGuide.class, firstArgValue);
-							type = "hg";
-						}
-					}
+                        if (ead == null) {
+                            // Checks if exists a HG with the provided identifier.
+                            if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                                // And repository code.
+                                ead = eadDAO.getEadByEadid(HoldingsGuide.class, secondArgValue, eadid, true);
+                                type = "hg";
+                            } else {
+                                // First HG with identifier.
+                                ead = eadDAO.getFirstPublishedEadByEadid(HoldingsGuide.class, eadid);
+                                type = "hg";
+                            }
+                        }
 
-					if (ead == null) {
-						// Checks if exists a SG with the provided identifier.
-						if (secondArgValue != null && !secondArgValue.isEmpty()) {
-							// And repository code.
-							ead = eadDAO.getEadByEadid(SourceGuide.class, secondArgValue, firstArgValue, true);
-							type = "sg";
-						} else {
-							// First SG with identifier.
-							ead = eadDAO.getFirstPublishedEadByEadid(SourceGuide.class, firstArgValue);
-							type = "sg";
-						}
-					}
-				}
+                        if (ead == null) {
+                            // Checks if exists a SG with the provided identifier.
+                            if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                                // And repository code.
+                                ead = eadDAO.getEadByEadid(SourceGuide.class, secondArgValue, eadid, true);
+                                type = "sg";
+                            } else {
+                                // First SG with identifier.
+                                ead = eadDAO.getFirstPublishedEadByEadid(SourceGuide.class, eadid);
+                                type = "sg";
+                            }
+                        }
+                    } else {
+                        // Checks if exists a FA with the provided identifier.
+                        if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                            // And repository code.
+                            ead = eadDAO.getEadByEadid(FindingAid.class, secondArgValue, firstArgValue, true);
+                            type = "fa";
+                        } else {
+                            // First FA with identifier.
+                            ead = eadDAO.getFirstPublishedEadByEadid(FindingAid.class, firstArgValue);
+                            type = "fa";
+                        }
 
-				if (ead != null) {
-					String repositoryCode = "";
-					if (secondArgValue != null && !secondArgValue.isEmpty()) {
-						repositoryCode = secondArgValue;
-					} else {
-						repositoryCode = ead.getArchivalInstitution().getRepositorycode();
-					}
+                        if (ead == null) {
+                            // Checks if exists a HG with the provided identifier.
+                            if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                                // And repository code.
+                                ead = eadDAO.getEadByEadid(HoldingsGuide.class, secondArgValue, firstArgValue, true);
+                                type = "hg";
+                            } else {
+                                // First HG with identifier.
+                                ead = eadDAO.getFirstPublishedEadByEadid(HoldingsGuide.class, firstArgValue);
+                                type = "hg";
+                            }
+                        }
 
-					value = "aicode/" + repositoryCode + "/type/" + type + "/id/" + ApeUtil.encodeSpecialCharacters(ead.getEadid());
-				}
+                        if (ead == null) {
+                            // Checks if exists a SG with the provided identifier.
+                            if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                                // And repository code.
+                                ead = eadDAO.getEadByEadid(SourceGuide.class, secondArgValue, firstArgValue, true);
+                                type = "sg";
+                            } else {
+                                // First SG with identifier.
+                                ead = eadDAO.getFirstPublishedEadByEadid(SourceGuide.class, firstArgValue);
+                                type = "sg";
+                            }
+                        }
 
-				return StringValue.makeStringValue(value);
-			} else {
-				return StringValue.makeStringValue("ERROR");
-			}
-		}
-		
-	}
+                        if (ead == null) {
+                            // Checks if a EAD3 with the provided identifier exists.
+                            if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                                // And repository code.
+                                ead3 = ead3DAO.getEad3ByIdentifier(secondArgValue, firstArgValue, true);
+                                type = "ead3";
+                            } else {
+                                // First FA with identifier.
+                                ead3 = ead3DAO.getFirstPublishedEad3ByIdentifier(firstArgValue, true);
+                                type = "ead3";
+                            }
+                        }
+                    }
+                }
+
+                if (ead != null) {
+                    String repositoryCode = "";
+                    if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                        repositoryCode = secondArgValue;
+                    } else {
+                        repositoryCode = ead.getArchivalInstitution().getRepositorycode();
+                    }
+
+                    value = "aicode/" + repositoryCode + "/type/" + type + "/id/" + ApeUtil.encodeSpecialCharacters(ead.getEadid());
+                } else if (ead3 != null) {
+                    String repositoryCode = "";
+                    if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                        repositoryCode = secondArgValue;
+                    } else {
+                        repositoryCode = ead3.getArchivalInstitution().getRepositorycode();
+                    }
+
+                    value = "aicode/" + repositoryCode + "/type/" + type + "/id/" + ApeUtil.encodeSpecialCharacters(ead3.getIdentifier());
+                }
+
+                return StringValue.makeStringValue(value);
+            } else {
+                return StringValue.makeStringValue("ERROR");
+            }
+        }
+
+    }
 
 }
